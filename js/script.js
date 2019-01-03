51ce2c6d189134aaf10c7f1d5a9dec0453a4ffcb
Anna Strzała <strzala.ania@gmail.com>
2019-01-03 14:33:43 +0100

    Add styles and some bug fix

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-'use strict';
+"use strict";
 
-var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-    fetch(prefix + quoteUrl, {
+    fetch(quoteUrl, {
             cache: "no-store"
         })
         .then(function (resp) {
@@ -25,16 +24,18 @@ function createTweet(input) {
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
-}
-var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 
-if (tweetText.length > 140) {
-    getQuote();
-} else {
-    var tweet = tweetLink + encodeURIComponent(tweetText);
-    document.querySelector('.quote').innerText = quoteText;
-    document.querySelector('.author').innerText = "Author: " + quoteAuthor;
-    document.querySelector('.tweet').setAttribute('href', tweet);
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        document.querySelector('.quote').innerText = quoteText;
+        document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+        document.querySelector('.tweet').setAttribute('href', tweet);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
